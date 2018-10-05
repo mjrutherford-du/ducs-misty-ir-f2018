48cdf5bd6efa65df2c35bfe618af94d4611ecbfc
Vince Luo <vinceluoindenver@gmail.com>
2018-10-05 16:54:17 -0600

    Now it can upload image and audio files to the robot

--- mistyFuncLib.py
+++ mistyFuncLib.py
@@ -8,7 +8,8 @@
 
 import json
 import requests
-import base64
+from gtts import gTTS
+import hashlib
 
 
 def change_led(ip_address, r, g, b):
@@ -59,55 +60,20 @@ def change_display_image(ip_address, file_name, time_out):
         print(response.text)
 
 
-def get_list_of_audio_clips(ip_address):
-    audio_list = []
-    url = "http://" + ip_address + "/api/audio/clips"
-    headers = {
-        'Content-Type': "application/json",
-        'Cache-Control': "no-cache"
-    }
-    response = requests.request("GET", url, headers=headers)
-    for i in response.json():
-        for j in i["result"]:
-            audio_list.append(j["name"])
-    # print(response.text)
-    # print(audio_list)
-    return audio_list
-
-
-def play_audio_clip(ip_address, file_name, volume):
-    if file_name not in get_list_of_audio_clips(ip_address):
-        raise Exception(file_name,"not found on the robot")
-    elif volume not in range(101):
-        raise Exception("Invalid volume value")
-    else:
-        url = "http://" + ip_address + "/api/audio/play"
-        payload = json.dumps(
-            {"AssetId": file_name, "Volume": volume})
-        headers = {
-            'Content-Type': "application/json",
-            'Cache-Control': "no-cache"
-        }
-        response = requests.request("POST", url, data=payload, headers=headers)
-        # print(response.text)
-
-
-# This function still has the problem about the byteArray.
-# The function can upload a file to the robot, but the screen
-#    will display "invalid image".
 def save_image_to_misty(image_name, ip_address):
     with open(image_name, "rb") as image_file:
-        encoded_string = bytearray(base64.b64encode(image_file.read()))
+        byte_array = bytearray(image_file.read())
+
     new_string = ""
-    for i in range(len(encoded_string)):
-        if i == (len(encoded_string) - 1):
-            new_string += str(encoded_string[i])
+    for i in range(len(byte_array)):
+        if i == (len(byte_array) - 1):
+            new_string += str(byte_array[i])
         else:
-            new_string += (str(encoded_string[i]) + ",")
+            new_string += (str(byte_array[i]) + ",")
 
     url = "http://"+ip_address+"/api/images"
     payload = json.dumps({"FileName": image_name, "DataAsByteArrayString": new_string, "Width": "480", "Height": "272",
-                          "ImmediatelyApply": False, "OverwriteExisting": True})
+                          "ImmediatelyApply": True, "OverwriteExisting": True})
     headers = {
         'Content-Type': "application/json",
         'Cache-Control': "no-cache"
@@ -127,6 +93,78 @@ def delete_image_asset_from_robot(image_name, ip_address):
     print(response.text)
 
 
+def get_list_of_audio_clips(ip_address):
+    audio_list = []
+    url = "http://" + ip_address + "/api/audio/clips"
+    headers = {
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache"
+    }
+    response = requests.request("GET", url, headers=headers)
+    for i in response.json():
+        for j in i["result"]:
+            audio_list.append(j["name"])
+    # print(response.text)
+    print(audio_list)
+    return audio_list
+
+
+def play_audio_clip(file_name, volume, ip_address):
+    if file_name not in get_list_of_audio_clips(ip_address):
+        raise Exception(file_name,"not found on the robot")
+    elif volume not in range(101):
+        raise Exception("Invalid volume value")
+    else:
+        url = "http://" + ip_address + "/api/audio/play"
+        payload = json.dumps(
+            {"AssetId": file_name, "Volume": volume})
+        headers = {
+            'Content-Type': "application/json",
+            'Cache-Control': "no-cache"
+        }
+        response = requests.request("POST", url, data=payload, headers=headers)
+        print(response.text)
+
+
+def save_audio_to_misty(audio_name, ip_address):
+    with open(audio_name, "rb") as audio_file:
+        byte_array = bytearray(audio_file.read())
+
+    new_string = ""
+    for i in range(len(byte_array)):
+        if i == (len(byte_array) - 1):
+            new_string += str(byte_array[i])
+        else:
+            new_string += (str(byte_array[i]) + ",")
+
+    url = "http://"+ip_address+"/api/audio"
+    payload = json.dumps({"FilenameWithoutPath": audio_name, "DataAsByteArrayString": new_string,
+                          "ImmediatelyApply": False, "OverwriteExisting": True})
+    headers = {
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+
+
+# Still have problem in this delete function
+def delete_audio_clip_from_robot(audio_name, ip_address):
+    url = "http://" + ip_address + "/api/audio/delete"
+    payload = json.dumps({"FileName": audio_name})
+    headers = {
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+
+
+def convert_text_to_audio(text):
+    tts = gTTS(text=text, lang='en')
+    tts.save(hashlib.md5(text).hexdigest()[:15]+".mp3")
+
+
 if __name__ == "__main__":
     ipAddress = "192.168.1.5"
     # change_led(ipAddress, 124, 40, 20)
@@ -134,5 +172,14 @@ if __name__ == "__main__":
     # change_display_image(ipAddress, "Angry.jpg", 10)
     # get_list_of_audio_clips(ipAddress)
     # play_audio_clip(ipAddress, "001-OooOooo.wav", 50)
-    # delete_image_asset_from_robot("test01.png", ipAddress)
+    # delete_image_asset_from_robot("test.png", ipAddress)
     # save_image_to_misty("test.png", ipAddress)
+    # save_audio_to_misty("test.mp3", ipAddress)
+    # get_list_of_audio_clips(ipAddress)
+    # play_audio_clip("test.mp3", 100, ipAddress)
+    # delete_audio_clip_from_robot("test.mp3", ipAddress)
+    # t = "Welcome to University of Denver"
+    # convert_text_to_audio(t)
+    # save_audio_to_misty(hashlib.md5(t).hexdigest()[:15]+".mp3", ipAddress)
+    # play_audio_clip(hashlib.md5(t).hexdigest()[:15]+".mp3", 100, ipAddress)
+    # delete_audio_clip_from_robot(hashlib.md5(t).hexdigest()[:15]+".mp3", ipAddress)
